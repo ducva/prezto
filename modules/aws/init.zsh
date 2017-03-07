73c7f50598b8c3bde56af424ac75f2fc07ef17a8
v-duc <v-duc@amida.co.jp>
2017-03-07 12:59:03 +0700

    show fullpath

--- modules/aws/init.zsh
+++ modules/aws/init.zsh
@@ -7,18 +7,18 @@
 #
 
 # Return if requirements are not found.
-# if (( ! $+commands[aws] )); then
-#   return 1
-# fi
+if (( ! $+commands[aws] )); then
+  return 1
+fi
 #
 # Aliases
 #
-export instanceId=i-02a4a2d28755947c3
-alias aws-get-p2='export instanceId=`aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped,Name=instance-type,Values=p2.xlarge" --query "Reservations[0].Instances[0].InstanceId"` && echo $instanceId'
 
-alias aws-get-t2='export instanceId=`aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped,Name=instance-type,Values=t2.micro" --query "Reservations[0].Instances[0].InstanceId"` && echo $instanceId'
-alias aws-start='aws ec2 start-instances --instance-ids $instanceId && aws ec2 wait instance-running --instance-ids $instanceId && export instanceIp=`aws ec2 describe-instances --filters "Name=instance-id,Values=$instanceId" --query "Reservations[0].Instances[0].PublicIpAddress"` && echo $instanceIp'
-alias aws-ip='export instanceIp=`aws ec2 describe-instances --filters "Name=instance-id,Values=$instanceId" --query "Reservations[0].Instances[0].PublicIpAddress"` && echo $instanceIp'
-alias aws-ssh='ssh -i /Users/trang/.ssh/aws-key-fast-ai.pem ubuntu@$instanceIp'
-alias aws-stop='aws ec2 stop-instances --instance-ids $instanceId'
+alias aws-get-p2='export instanceId=`aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped,Name=instance-type,Values=p2.xlarge" --query "Reservations[0].Instances[0].InstanceId"` && echo $AWS_INSTANCE_ID'
+
+alias aws-get-t2='export instanceId=`aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped,Name=instance-type,Values=t2.micro" --query "Reservations[0].Instances[0].InstanceId"` && echo $AWS_INSTANCE_ID'
+alias aws-start='aws ec2 start-instances --instance-ids $AWS_INSTANCE_ID && aws ec2 wait instance-running --instance-ids $AWS_INSTANCE_ID && export instanceIp=`aws ec2 describe-instances --filters "Name=instance-id,Values=$AWS_INSTANCE_ID" --query "Reservations[0].Instances[0].PublicIpAddress"` && echo $instanceIp'
+alias aws-ip='export instanceIp=`aws ec2 describe-instances --filters "Name=instance-id,Values=$AWS_INSTANCE_ID" --query "Reservations[0].Instances[0].PublicIpAddress" --output text` && echo $instanceIp'
+alias aws-ssh='ssh -i $PRIVATE_KEY ubuntu@$instanceIp'
+alias aws-stop='aws ec2 stop-instances --instance-ids $AWS_INSTANCE_ID'
 
